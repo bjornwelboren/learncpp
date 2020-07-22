a0d24cc73546f42ef5ec2999a9b6b397b9e3aff2
Björn <52087548+bjornwelboren@users.noreply.github.com>
2020-07-22 14:04:34 +0200

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void shortWay();
 void longWay();
 void messing();
+void z();
 
 int main(){
   shortWay();
@@ -31,4 +32,17 @@ void messing(){
   *ptr = 1337;
   *(&randomValue) = 20;
   cout << randomValue << endl << randomValue2 << endl; // 20 1337 
+}
+
+void z(){
+  //Ways of iterating through an array via memory and pointers
+  int numbers[5];
+  int * p;
+  p = numbers;  *p = 10;
+  p++;  *p = 20;
+  p = &numbers[2];  *p = 30;
+  p = numbers + 3;  *p = 40;
+  p = numbers;  *(p+4) = 50;
+  for (int n=0; n<5; n++)
+    cout << numbers[n] << ", ";
 }
