5356a3c6696f77c0fc4f9123068476a27b5cbc7d
Björn <52087548+bjornwelboren@users.noreply.github.com>
2020-07-22 14:01:57 +0200

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 void shortWay();
 void longWay();
+void messing();
 
 int main(){
   shortWay();
@@ -20,4 +21,14 @@ void longWay(){
   ptr = &a;
   *ptr = 20;
   cout << a;
+}
+
+void messing(){
+  int randomValue (10);
+  int randomValue2 (20);
+  int * ptr;
+  ptr = &randomValue2;
+  *ptr = 1337;
+  *(&randomValue) = 20;
+  cout << randomValue << endl << randomValue2 << endl; // 20 1337 
 }
