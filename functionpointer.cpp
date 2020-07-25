ded5caa7e4fab772531d9ba7fe36a875678815d9
Björn <52087548+bjornwelboren@users.noreply.github.com>
2020-07-25 16:20:02 +0200

    simplified

--- functionpointer.cpp
+++ functionpointer.cpp
@@ -1,28 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int addition(int a, int b){
-    return (a+b);
-}
-
-int subtraction(int a, int b){
-    return (a-b);
-}
-
-// handles a function
-int operation(int a, int b, int (*functocall)(int, int)){
-    int g;
-    g = (*functocall)(a,b);
-    return (g);
-}
+int subtraction(int a, int b){return (a - b);};
+int operation(int a, int b, int(*functocall)(int,int)){
+	int g;
+	g = (*functocall)(a, b);
+	return (g);
+};
 
 int main(){
-    int a, b;
-    int (*minus)(int,int) = subtraction;
-    
-    a = operation(10,5, minus); 
-    b = operation(10,5, addition); 
-    cout << a << endl << b << endl;
-    
+  int a;
+	int(*minus)(int,int) = subtraction;
+  a = operation(999,333, minus);
+	cout << a;
 }
-
