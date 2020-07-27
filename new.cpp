33f6da8d4647e8871b81aefc678aa4ab0c2ff67a
Björn <52087548+bjornwelboren@users.noreply.github.com>
2020-07-27 16:09:53 +0200

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -4,5 +4,16 @@ using namespace std;
 int main(){
   int * foo;
   foo = new int [5];
+  foo = new (nothrow) int [5];
+  ++(*foo);
+       
+  if(foo == nullptr){
+     cout << "Caught exception" << endl;
+     return 1;
+  } else {
+     cout << foo[0] << endl;  
+  }
+  
+  return 0;
 
 }
