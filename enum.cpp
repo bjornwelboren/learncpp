064f8bab56b4ebd344204138e9428926fb2f4078
вנσяи <52087548+bjornwelboren@users.noreply.github.com>
2020-08-01 17:03:15 +0200

    custom datatype with enum

---
+++ enum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+enum colors_t{red,white,blue);
+
+int main(){
+  colors_t mycolor;
+  mycolor = red;
+  if(mycolor == white) mycolor = blue;
+  return 0;
+}
