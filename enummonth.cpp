8b836e2048d3d866df6bb3337ba5ee7fbaef7b37
вנσяи <52087548+bjornwelboren@users.noreply.github.com>
2020-08-01 20:12:15 +0200

    Create enummonth.cpp

---
+++ enummonth.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+enum months_t{january=1, february, march, april} y2k;
+
+int main(){
+    
+    months_t month;
+    month = february;
+    cout << month << endl; //2 
+    
+    
+    return 0;
+}
