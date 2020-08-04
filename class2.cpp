79d46ac87b2e1f6e8bd51555a5258dddcacc85f4
вנσяи <52087548+bjornwelboren@users.noreply.github.com>
2020-08-05 01:28:42 +0200

    Update class2.cpp

--- class2.cpp
+++ class2.cpp
@@ -1,24 +1,27 @@
-// example: one class, two objects
 #include <iostream>
 using namespace std;
 
-class Rectangle {
-    int width, height;
-  public:
-    void set_values (int,int);
-    int area () {return width*height;}
+class Rectangle{
+    float width, height;
+public:
+    Rectangle(float, float); //Constructor
+    int area(){ return width*height;};
+    
 };
 
-void Rectangle::set_values (int x, int y) {
-  width = x;
-  height = y;
+Rectangle::Rectangle(float x, float y){
+    width = x;
+    height = y;
 }
 
-int main () {
-  Rectangle rect, rectb;
-  rect.set_values (3,4);
-  rectb.set_values (5,6);
-  cout << "rect area: " << rect.area() << endl;
-  cout << "rectb area: " << rectb.area() << endl;
-  return 0;
+int main(){
+    
+    Rectangle rx(2, 668.5);
+    cout << rx.area() << endl;
+    Rectangle rx2(2, 333);
+    cout << rx2.area() << endl;
+    
+    
+    return 0;
+    
 }
