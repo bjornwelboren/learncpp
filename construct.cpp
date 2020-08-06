7fd62c999bb2c71dc650b73e04e99dabf7f34186
вנσяи <52087548+bjornwelboren@users.noreply.github.com>
2020-08-06 15:57:23 +0200

    Update construct.cpp

--- construct.cpp
+++ construct.cpp
@@ -5,19 +5,23 @@ using namespace std;
 class Circle{
     int heightValue;
 public:
+    Circle();
     Circle(int);
     int height(){return heightValue;};
 };
 
+Circle::Circle(){ //Default value
+    heightValue = 1337;
+}
 
-Circle::Circle(int x){
+Circle::Circle(int x){ //Optional value to set the height as
     heightValue = x;
 }
 
 
 int main(){
     
-    Circle circle(3);
+    Circle circle;
     cout << circle.height() << endl;
     
 
