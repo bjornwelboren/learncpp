57526ed854d783fdad2cfe7976a4098b14f23df5
вנσяи <52087548+bjornwelboren@users.noreply.github.com>
2020-08-14 14:22:20 +0200

    Update this.cpp

--- this.cpp
+++ this.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 using namespace std;
 
+
 class Dummy{
 public:
   bool isitme(Dummy& param);
 };
 
-Dummy::isitme(Dummy& param){
+bool Dummy::isitme(Dummy& param){
   if(&param == this) return true;
   else return false;
 }
 
 int main(){
-  Dummy a;
-  Dummy *b;
-  b = &a;
-  if(b->isitme(a)) cout << "Yes &a is b" << endl;
+
+  Dummy foo;
+  Dummy * ptr;
+  ptr = &foo;
+  if(ptr->isitme(foo)){
+    cout << "Yes its me " << endl;
+  } else {
+    cout << "Nope it isnt" << endl;
+  }
+
+  return 0;
 }
