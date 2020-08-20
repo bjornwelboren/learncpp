58460445cc58e92497fd2abf021a967f8c9e67b0
вנσяи <52087548+bjornwelboren@users.noreply.github.com>
2020-08-20 21:48:29 +0200

    Create stuffz.cc

---
+++ stuffz.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <memory>
+#include <string>
+using namespace std;
+
+class Dummy{
+public:
+    int salary = 1700;
+    Dummy() {}
+    
+};
+
+void mystery(const string* casted){
+    cout << *casted << endl;
+}
+
+int main(){
+    int arraySize = 8;
+    int * newArray = new int[3];
+    ++(*newArray); //Move in value 
+    *newArray = 1;
+    ++(*newArray);
+    ++newArray;
+    *newArray = 3;
+    cout << *newArray << endl; //3
+    --newArray; //Move in memory
+    cout << *newArray << endl; //2
+    Dummy* dmy = new Dummy; //Standard usage
+    auto dmyy = make_unique<Dummy>();//Smart pointer 
+    auto dmyyy = make_unique<Dummy[]>(5);
+    cout << dmyy->salary << endl;
+    dmyyy[1].salary = 5;
+    cout << dmyyy[1].salary << endl;
+    string mystring = "Bjorn";
+    mystery(&mystring);
+    
+}
